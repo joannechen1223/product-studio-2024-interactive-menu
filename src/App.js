375a6df2c7d6533911880471a95729693f26976b
joannechen1223 <joannechen1223@gmail.com>
2024-11-11 20:53:21 -0500

    fix: change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import Camera from "./pages/Camera";
 import Home from "./pages/Home";
 import Onboarding from "./pages/Onboarding";
 function App() {
   return (
-    <BrowserRouter basename="/product-studio-2024-interactive-menu">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/onboarding" element={<Onboarding />} />
         <Route path="/camera" element={<Camera />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
